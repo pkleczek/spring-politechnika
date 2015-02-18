1e7207a8672d224392998e4996d042c96729aad1
Paweł Kłeczek <pkleczek@gmail.com>
2015-02-18 22:49:48 +0100

    Sample spring boot project

---
+++ spring-boot-demo/src/main/java/pl/edu/prz/spring/boot/SpringBootDemoApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.prz.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
